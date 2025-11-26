8b4875d688b3518dbf5f225977188e5de4d19e81
lasse <lasse.maile@tum.de>
2025-11-26 21:52:33 +0100

    added fetch_gtfs.py which fetches and unzips data from link

---
+++ fetch_gtfs.py
@@ -0,0 +1,21 @@
+from urllib.request import urlretrieve
+import zipfile
+import os
+
+def download_gtfs_data(url: str="https://download.gtfs.de/germany/free/latest.zip",
+                       filename: str="de_full.zip",
+                       keep_zip: bool = False):
+    """
+    fetches base gtfs data
+    :param url: url of gtfs data to be fetched
+    :param filename: name of folder gtfs data is to be stored
+    :param keep_zip: decides if downloaded zip archive should be kept or deleted after unzipping
+    :return:
+    """
+
+    urlretrieve(url,filename)
+
+    with zipfile.ZipFile(filename, 'r') as zip:
+        zip.extractall(filename[:-4])
+    if not keep_zip:
+        os.remove(filename)
