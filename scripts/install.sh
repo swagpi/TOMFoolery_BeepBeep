4d672634b15a05e5d5f65d48b18736e492838b77
Ben Kirschniak <ben@kirschniak.de>
2025-11-28 17:21:45 +0100

    some config

---
+++ scripts/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+CURRENT_DIR = $PWD
+SERVICE_DIR = $PWD/services
+SERVICES = $(find $SERVICE_DIR -n "*.service")
+
+SYSTEMD_CONF="/etc/systemd/system.conf"
+BACKUP_CONF="/etc/systemd/system.conf.bak"
+
+echo "Backing up systemd config to $BACKUP_CONF..."
+sudo cp "$SYSTEMD_CONF" "$BACKUP_CONF"
+
+# Append custom UnitPath if not already present
+# if ! grep -q "$SERVICE_DIR" "$SYSTEMD_CONF"; then
+#     echo "Adding custom service path to systemd..."
+#     sudo sed -i "/^\[Manager\]/a UnitPath=/etc/systemd/system:/lib/systemd/system:$SERVICE_DIR" "$SYSTEMD_CONF"
+# fi
+
+if ! [[ -d /etc/systemd/system.conf.d ]]; then
+    sudo mkdir /etc/systemd/system.conf.d
+fi
+
+if ! [[ -f /etc/systemd/system.conf.d/tomfoolery.conf ]]; then 
+    echo "[Manager]\nUnitPath=/etc/systemd/system:/lib/systemd/system:$SERVICE_DIR" > /etc/systemd/system.conf.d/tomfoolery.conf
+fi
+
+echo "Reloading systemd daemon..."
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+
+for SERVICE in $SERVICES do
+    sudo systemctl enable $SERVICE
+done
