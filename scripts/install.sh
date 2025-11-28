08c816ca5cc43459489273c212664e0a9edd0b17
Ben Kirschniak <ben@kirschniak.de>
2025-11-28 18:15:38 +0100

    install refactoring

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,15 +10,6 @@ SERVICES=$(find $SERVICE_DIR -name "*.service")
 SYSTEMD_CONF="$HOME/.config/systemd/user/system.conf"
 BACKUP_CONF="/etc/systemd/system.conf.bak"
 
-# echo "Backing up systemd config to $BACKUP_CONF..."
-# sudo cp "$SYSTEMD_CONF" "$BACKUP_CONF"
-
-# Append custom UnitPath if not already present
-# if ! grep -q "$SERVICE_DIR" "$SYSTEMD_CONF"; then
-#     echo "Adding custom service path to systemd..."
-#     sudo sed -i "/^\[Manager\]/a UnitPath=/etc/systemd/system:/lib/systemd/system:$SERVICE_DIR" "$SYSTEMD_CONF"
-# fi
-
 SYSTEMD_CONF_FILE="
 [Manager]
 UnitPath=/etc/systemd/system:/lib/systemd/system:$SERVICE_DIR
@@ -26,7 +17,7 @@ UnitPath=/etc/systemd/system:/lib/systemd/system:$SERVICE_DIR
 
 if ! [[ -d $USER_SERVICE_DIR ]]; then
     echo "creating user service dir"
-    sudo mkdir $USER_SERVICE_DIR
+    mkdir -p $USER_SERVICE_DIR
 fi
 
 echo "Reloading systemd daemon..."
@@ -37,7 +28,11 @@ for SERVICE in $SERVICES do
     if ! [[ -f $USER_SERVICE_DIR/$(basename $SERVICE) ]]; then
         cp $SERVICE $USER_SERVICE_DIR/$(basename $SERVICE)
     fi
+done
 
+for SERVICE in $SERVICES do
+    echo "Enabling: $(basename $SERVICE)"
     systemctl --user enable $(basename $SERVICE)
     systemctl --user start $(basename $SERVICE)
+    echo "Enabled: $(basename $SERVICE)"
 done
